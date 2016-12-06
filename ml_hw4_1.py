614760602a574e2319afbd434c80490a128c8d84
Mohammadhossein <mhgg.mhgg@gmail.com>
2016-12-07 01:37:08 +0330

    update_centers function added.

--- ml_hw4_1.py
+++ ml_hw4_1.py
@@ -17,19 +17,26 @@ g_BLUE_INDEX = 2
 false = False
 true = True
 
+# <editor-fold desc="Output functions">
 file_output = open(g_OUTPUT_FILE, 'w')
+
+
 def printl(obj):
     orig_stdout = sys.stdout
     sys.stdout = file_output
     print obj
     sys.stdout = orig_stdout
     print obj
+
+
 def print_(obj):
     orig_stdout = sys.stdout
     sys.stdout = file_output
     print obj,
     sys.stdout = orig_stdout
     print obj,
+# </editor-fold>
+
 
 # <editor-fold desc="Image declaration">
 class Image:
@@ -58,11 +65,12 @@ class Image:
 
     def instance(self, row, col):
         sel_rgb = self.image[row, col]
-        return [ sel_rgb[g_RED_INDEX], sel_rgb[g_GREEN_INDEX], sel_rgb[g_BLUE_INDEX] ]
+        return [sel_rgb[g_RED_INDEX], sel_rgb[g_GREEN_INDEX], sel_rgb[g_BLUE_INDEX]]
 # </editor-fold>
 
-def get_labels(obj, clusters):
-    labels = [[]]
+
+def set_labels(obj, clusters):
+    labels = []
     row_index = -1
     for row in obj.image:
         row_index += 1
@@ -89,6 +97,40 @@ def get_labels(obj, clusters):
     #     printl('] ')
     return labels
 
+
+def update_centers(obj, labels, clusters):
+    # old_clusters = [clusters[i][:] for i in range(len(clusters))]
+    # clusters = [[0, 0, 0] for i in range(len(clusters))]
+    # clusters_count = [0 for i in range(len(clusters))]
+
+    new_clusters = []
+    clusters_count = []
+    for i in range(len(clusters)):
+        new_clusters.append([0, 0, 0])
+        clusters_count.append(0)
+
+    print "new_clusters: \n", new_clusters
+    print "old_clusters: \n", clusters
+
+    row_index = -1
+    for row in obj.image:
+        cell_index = -1
+        row_index += 1
+        for cell in row:
+            cell_index += 1
+            c = labels[row_index][cell_index]
+            new_clusters[c] += cell
+            clusters_count[c] += 1
+    cluster_index = -1
+    for cluster in new_clusters:
+        cluster_index += 1
+        cluster /= ([clusters_count[cluster_index]] * 3)
+    # for i in range(clusters):
+    #     clusters[i] /= ([clusters_count[i]] * 3)
+    print "new_clusters: \n", new_clusters
+    return new_clusters
+
+
 def k_means(obj):
     print_("k-means started...")
 
@@ -98,30 +140,34 @@ def k_means(obj):
         r, c = obj.size()
         rand_r = random.randint(0, r)
         rand_c = random.randint(0, c)
-        clusters.append( obj.instance(rand_r, rand_c) )
+        clusters.append(obj.instance(rand_r, rand_c))
 
     # Converging loop
     iter = 0
     old_clusters = None
     should_stop = false
     while not should_stop:
-        old_clusters = clusters
+        old_clusters = [[row[g_RED_INDEX], row[g_GREEN_INDEX], row[g_BLUE_INDEX]] for row in clusters]
         iter += 1
 
-        get_labels(obj, clusters)
+        # Set label for every point
+        labels = set_labels(obj, clusters)
 
-        if (iter > g_MAX_ITERATION) or (old_clusters == clusters):
+        # Update center of clusters
+        clusters = update_centers(obj, labels, clusters)
+
+        if (iter > g_MAX_ITERATION) or np.all([old_clusters[i]] == clusters[i] for i in range(g_CLUSTER_COUNT)):
             should_stop = true
     print_("...finished")
 
-ds = Image(misc.imread(g_TEST))
+img = Image(misc.imread(g_TEST))
 
-plt.imshow(ds.image)
+plt.imshow(img.image)
 plt.show()
 
-k_means(ds)
+k_means(img)
 
-plt.imshow(ds.image)
+plt.imshow(img.image)
 plt.show()
 
 file_output.close()
